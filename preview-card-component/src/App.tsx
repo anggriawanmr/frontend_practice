be7a1742c6c9ad7d9650535c4477992f57fbd204
anggriawanmr <anggriawanmr@gmail.com>
2023-02-22 16:13:05 +0700

    Finishing :tada:

--- preview-card-component/src/App.tsx
+++ preview-card-component/src/App.tsx
@@ -1,4 +1,5 @@
 import image from './assets/image-product-desktop.jpg';
+import cart from './assets/icon-cart.svg';
 import './App.css';
 
 function App() {
@@ -10,14 +11,16 @@ function App() {
       <div className="right-content">
         <h3 className="subtitle">perfume</h3>
         <h1 className="title">Gabrielle Essence Eau De Parfum</h1>
-        <p className="paragraph">
+        <p className="description">
           A floral, solar and voluptuous interpretation composed by Olivier
           Polge, Perfumer-Creator for the House of CHANEL
         </p>
         <div className="price">
           $149.99 <span>$169.99</span>
         </div>
-        <button>Add to Cart</button>
+        <button className="btn">
+          <img src={cart} alt="cart" /> Add to Cart
+        </button>
       </div>
     </div>
   );
