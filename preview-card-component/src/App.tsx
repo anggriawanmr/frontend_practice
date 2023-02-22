7bb986bac615ec84de9b6a5cf87bcd649b9be41e
anggriawanmr <anggriawanmr@gmail.com>
2023-02-22 15:32:15 +0700

    Adding image and editing the column :hammer:

--- preview-card-component/src/App.tsx
+++ preview-card-component/src/App.tsx
@@ -1,34 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import image from './assets/image-product-desktop.jpg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+    <div className="container">
+      <div className="left-content">
+        <img src={image} alt="product" />
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+      <div className="right-content">
+        <h3 className="subtitle">perfume</h3>
+        <h1 className="title">Gabrielle Essence Eau De Parfum</h1>
+        <p className="paragraph">
+          A floral, solar and voluptuous interpretation composed by Olivier
+          Polge, Perfumer-Creator for the House of CHANEL
         </p>
+        <div className="price">
+          $149.99 <span>$169.99</span>
+        </div>
+        <button>Add to Cart</button>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
