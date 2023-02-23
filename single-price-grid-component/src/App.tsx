abf26503d0a3a419bebf54ca01cd2029e511de8c
anggriawanmr <anggriawanmr@gmail.com>
2023-02-23 10:15:53 +0700

    Finalizing single price grid component :hammer:

--- single-price-grid-component/src/App.tsx
+++ single-price-grid-component/src/App.tsx
@@ -1,34 +1,45 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
+    <div className="container">
+      <div className="top-container">
+        <h1 className="title">Join our community</h1>
+        <h3 className="sub-title">30-day, hassle-free money back guarantee</h3>
         <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
+          Gain access to our full library of tutorials along with expert code
+          reviews. Perfect for any developers who are srious about honing their
+          skills.
         </p>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <div className="bottom-container">
+        <div className="left-content">
+          <h3>Monthly Subscription</h3>
+          <h1>
+            $29 <span>per month</span>
+          </h1>
+          <p>Full access for less than $1 a day</p>
+          <button className="btn">Sign Up</button>
+        </div>
+        <div className="right-content">
+          <h3>Why Us</h3>
+          <ul>
+            <li>Tutorials by industry experts</li>
+            <li>Peer & expert code review</li>
+            <li>Coding exercises</li>
+            <li>Access to our GitHub repos</li>
+            <li>Community forum</li>
+            <li>Flashcard decks</li>
+            <li>New videos every week</li>
+          </ul>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
