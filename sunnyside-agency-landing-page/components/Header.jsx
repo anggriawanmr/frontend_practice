3dce49c10c039abd1977e78edc24fbce8458542b
anggriawanmr <anggriawanmr@gmail.com>
2023-05-25 17:26:51 +0700

    Finalizing Header :hammer:

--- sunnyside-agency-landing-page/components/Header.jsx
+++ sunnyside-agency-landing-page/components/Header.jsx
@@ -2,13 +2,26 @@ import Image from 'next/image';
 
 const Header = () => {
   return (
-    <div className="absolute overflow-hidden bg-cover h-screen w-100vh bg-center bg-no-repeat">
+    <div className="header">
       <Image
         src="/assets/images/image-header.jpg"
         alt="hero-image"
         width={2000}
         height={2000}
+        className="absolute top-0 left-0 h-[100vh] z-[-10px]"
       />
+      <div></div>
+      <h1 className="heading__header">We Are Creatives</h1>
+
+      <Image
+        src="/assets/icon-arrow-down.svg"
+        alt="arrow-header"
+        width={1000}
+        height={1000}
+        className="w-5  h-20 z-20"
+      />
+      <div></div>
+      <div></div>
     </div>
   );
 };
--- sunnyside-agency-landing-page/components/Navbar.jsx
+++ sunnyside-agency-landing-page/components/Navbar.jsx
@@ -9,15 +9,15 @@ const Navbar = () => {
           width={100}
           height={100}
           alt="logo"
-          className="bg-[#999] w-[100%] h-10"
+          className="w-[100%] h-10"
         />
       </div>
 
       <div className="navLinks">
         <li className="navbar__menu">
-          <ul>About</ul>
-          <ul>Services</ul>
-          <ul>Projects</ul>
+          <ul className="navbar__menu-items">About</ul>
+          <ul className="navbar__menu-items">Services</ul>
+          <ul className="navbar__menu-items">Projects</ul>
         </li>
         <button className="navbar__button">CONTACT</button>
       </div>
