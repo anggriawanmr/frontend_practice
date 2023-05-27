57e660ab19b13dd6dd636e36bee620bbbf98d9f2
anggriawanmr <anggriawanmr@gmail.com>
2023-05-27 18:22:16 +0700

    Editing form :hammer:

--- age-calculator/app/page.jsx
+++ age-calculator/app/page.jsx
@@ -1,5 +1,28 @@
+import Image from 'next/image';
+
 const Home = () => {
-  return <section>test</section>;
+  return (
+    <section className="home">
+      <div className="block-content">
+        <div className="date-input">
+          <form>
+            <label>Day</label>
+            <input type="number" className="date-input__block"></input>
+          </form>
+          <form>
+            <label>Month</label>
+            <input type="number" className="date-input__block"></input>
+          </form>
+          <form>
+            <label>Year</label>
+            <input type="number" className="date-input__block"></input>
+          </form>
+        </div>
+
+        <br />
+      </div>
+    </section>
+  );
 };
 
 export default Home;
